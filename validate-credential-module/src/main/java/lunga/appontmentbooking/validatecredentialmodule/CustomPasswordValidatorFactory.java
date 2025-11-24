cc5bb6df7d8109e8405d6bf6c87f445be17d8abb
Lunga Tsewu <1998tsewu@gmail.com>
2025-11-24 21:28:55 +0200

    Added spi module to verify current pasword

---
+++ validate-credential-module/src/main/java/lunga/appontmentbooking/validatecredentialmodule/CustomPasswordValidatorFactory.java
@@ -0,0 +1,35 @@
+package lunga.appontmentbooking.validatecredentialmodule;
+
+import lunga.appontmentbooking.validatecredentialmodule.api.CustomPasswordValidateCurrentPassword;
+import org.keycloak.Config;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.KeycloakSessionFactory;
+import org.keycloak.services.resource.RealmResourceProvider;
+import org.keycloak.services.resource.RealmResourceProviderFactory;
+
+
+public class CustomPasswordValidatorFactory implements RealmResourceProviderFactory {
+    public static final String ID = "verify";
+
+    @Override
+    public RealmResourceProvider create(KeycloakSession session) {
+        return new CustomPasswordValidateCurrentPassword(session);
+    }
+
+    @Override
+    public void init(Config.Scope scope) {
+    }
+
+    @Override
+    public void postInit(KeycloakSessionFactory keycloakSessionFactory) {
+
+    }
+
+    @Override
+    public String getId() {
+        return ID;
+    }
+
+    @Override
+    public void close() {}
+}
