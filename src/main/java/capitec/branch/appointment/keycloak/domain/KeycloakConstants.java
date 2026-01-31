735f1f5cc27f2d9cf9b3e23ea4f04ff09b219c13
tswlun002 <1998tsewu@gmail.com>
2026-01-31 21:40:29 +0200

    Added request token type in the impersonation body

--- src/main/java/capitec/branch/appointment/keycloak/domain/KeycloakConstants.java
+++ src/main/java/capitec/branch/appointment/keycloak/domain/KeycloakConstants.java
@@ -4,6 +4,7 @@ public final class KeycloakConstants {
     public static final String GRANT_TYPE_PASSWORD = "password";
     public static final String GRANT_TYPE_REFRESH = "refresh_token";
     public static final String GRANT_TYPE_TOKEN_EXCHANGE = "urn:ietf:params:oauth:grant-type:token-exchange";
+    public static final String REQUESTED_TOKEN_TYPE= "urn:ietf:params:oauth:token-type:refresh_token";
     public static final String SUBJECT_TOKEN_TYPE = "urn:ietf:params:oauth:token-type:access_token";
     public static final String DEFAULT_SCOPE = "openid profile email";
     
