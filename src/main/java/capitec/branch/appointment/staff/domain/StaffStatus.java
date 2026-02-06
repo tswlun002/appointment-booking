80b87239b36a621499a56b39236957e0174f2db8
tswlun002 <1998tsewu@gmail.com>
2026-02-07 00:02:00 +0200

    Updated state management  validation

--- src/main/java/capitec/branch/appointment/staff/domain/StaffStatus.java
+++ src/main/java/capitec/branch/appointment/staff/domain/StaffStatus.java
@@ -3,16 +3,24 @@ package capitec.branch.appointment.staff.domain;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 import org.apache.commons.lang3.StringUtils;
-
-import java.util.Arrays;
-
 public enum StaffStatus{
     TRAINING,WORKING, LEAVE;
+
+    public static boolean isValid(String value){
+        for(StaffStatus staffStatus : StaffStatus.values()){
+            if(staffStatus.name().equals(value)){
+                return true;
+            }
+        }
+        return false;
+    }
 
     record IsStaffStatusValidator() implements ConstraintValidator<IsStaffStatus, String> {
         @Override
         public boolean isValid(String status, ConstraintValidatorContext constraintValidatorContext) {
-            return StringUtils.isNotBlank(status) && Arrays.asList(StaffStatus.values()).contains(status);
+
+                return StringUtils.isNotBlank(status) && StaffStatus.isValid(status) ;
+
         }
     }
 }
