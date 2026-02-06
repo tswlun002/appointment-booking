a005ea553089c1b81843a48f9a0a8460e5a52472
tswlun002 <1998tsewu@gmail.com>
2026-02-06 23:57:19 +0200

    Added  authorization to end-point

--- src/main/java/capitec/branch/appointment/role/infrastructure/api/RolesController.java
+++ src/main/java/capitec/branch/appointment/role/infrastructure/api/RolesController.java
@@ -18,7 +18,7 @@ import java.util.Collection;
 import java.util.Set;
 
 @RestController
-@RequestMapping("users-service/roles")
+@RequestMapping("api/v1/roles")
 @RequiredArgsConstructor
 @Validated
 @Log4j2
@@ -27,18 +27,21 @@ public class RolesController {
     private final UserRoleUseCase userRoleUseCase;
 
     @PostMapping("/client/create")
+    @PreAuthorize("hasAnyRole('app_admin')")
     public ResponseEntity<?> createRole(@Valid @RequestBody RoleDTO roleDTO, @RequestHeader("Trace-Id") String traceId) {
         log.info("Role {}, traceId:{}", roleDTO, traceId);
         roleManagementUseCase.createClientRole(roleDTO, traceId);
         return new ResponseEntity<>("Role created successfully",HttpStatus.CREATED);
     }
     @GetMapping("/client")
+    @PreAuthorize("hasAnyRole('app_admin')")
     public ResponseEntity<?> getRole( @RequestParam("roleName") String roleName, @RequestHeader("Trace-Id") String traceId) {
         log.info("Fetch role {}, traceId:{}", roleName, traceId);
         Role clientRole = roleManagementUseCase.getClientRole(roleName, traceId);
         return new ResponseEntity<>(clientRole,HttpStatus.OK);
     }
     @DeleteMapping("/client/delete/{roleId}")
+    @PreAuthorize("hasAnyRole('app_admin')")
     public ResponseEntity<?> deleteRole( @PathVariable("roleId") String roleId, @RequestHeader("Trace-Id") String traceId) {
         log.info("Delete role {}, traceId:{}", roleId, traceId);
         roleManagementUseCase.deleteClientRole(roleId, traceId);
@@ -46,7 +49,7 @@ public class RolesController {
     }
 
     @PutMapping("/client/assign/role/{roleId}/user/{username}")
-    @PreAuthorize("!@securityUtils.isusernameMatching(authentication, #username)")  //user cannot assign themselves role
+    @PreAuthorize("'app_admin' and !@securityUtils.isusernameMatching(authentication, #username)")  //user cannot assign themselves role
     public ResponseEntity<?> assignClientRoleToUser(@PathVariable("roleId") String roleId,
                                                     @PathVariable("username") String username,
                                                     @RequestHeader("Trace-Id") String traceId) {
@@ -56,6 +59,7 @@ public class RolesController {
     }
 
     @GetMapping("/client/user/{username}")
+    @PreAuthorize("hasAnyRole('app_admin')")
     public ResponseEntity<?> getUserClientRoles(@PathVariable("username") String username, @RequestHeader("Trace-Id") String traceId) {
         log.info("Fetch user:{} client roles, traceId:{}",username, traceId);
         Collection<String> clientRolesForUser = userRoleUseCase.getClientRolesForUser(username, traceId);
@@ -63,6 +67,7 @@ public class RolesController {
     }
 
     @PostMapping("/group/create")
+    @PreAuthorize("hasAnyRole('app_admin')")
     public ResponseEntity<?> createRoleType(@Valid @RequestBody RoleTypeDTO roleTypeDTO, @RequestHeader("Trace-Id") String traceId) {
         log.info("Create role:{} type {}", roleTypeDTO,roleTypeDTO);
         roleManagementUseCase.createRoleType(roleTypeDTO.name(),traceId);
@@ -70,6 +75,7 @@ public class RolesController {
     }
 
     @GetMapping("/group")
+    @PreAuthorize("hasAnyRole('app_admin')")
     public ResponseEntity<?> getRoleType(@RequestParam("name") String name,@RequestParam(value = "withRole",required = false) Boolean withRole ,@RequestHeader("Trace-Id") String traceId) {
         log.info("Fetch role type:{} traceId:{}", name,traceId);
         RoleType roleTypeWithRoles = roleManagementUseCase.getRoleType(name,withRole, traceId);
@@ -78,6 +84,7 @@ public class RolesController {
     }
 
     @PutMapping("/group/{groupId}/assign/roles")
+    @PreAuthorize("hasAnyRole('app_admin')")
     public  ResponseEntity<?> assignRoleToGroup(@PathVariable("groupId")String groupId, @RequestBody Set<String> roleIds, @RequestHeader("Trace-Id") String traceId) {
         log.info("Assign role to group {}, to role:{} traceId:{}", String.join(", ",groupId), roleIds, traceId);
         roleIds.forEach(roleId-> roleManagementUseCase.assignRoleToToGroup(groupId, roleId,traceId));
