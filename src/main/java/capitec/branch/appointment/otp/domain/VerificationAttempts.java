7667213eda3fcd68d63c95937cb7abe161146e3d
tswlun002 <1998tsewu@gmail.com>
2026-02-10 02:21:52 +0200

    Remove db dependencies on domain

--- src/main/java/capitec/branch/appointment/otp/domain/VerificationAttempts.java
+++ src/main/java/capitec/branch/appointment/otp/domain/VerificationAttempts.java
@@ -3,15 +3,11 @@ package capitec.branch.appointment.otp.domain;
 import jakarta.ws.rs.InternalServerErrorException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.annotation.Transient;
-import org.springframework.data.relational.core.mapping.Column;
 
 @Slf4j
 public record VerificationAttempts(
-        @Column("verification_attempts")
         int attempts,
         @Value("${otp.number.verification.attempts}")
-        @Transient
         int maxNumberVerificationAttempts
 ) {
     public  VerificationAttempts {
