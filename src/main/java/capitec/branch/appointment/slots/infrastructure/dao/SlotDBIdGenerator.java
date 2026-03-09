331179b7b6fd5d636b105395a52fcb8485f3db4a
tswlun002 <1998tsewu@gmail.com>
2026-03-10 01:11:58 +0200

    Update: add logging for ID generation and updates in SlotDBIdGenerator

--- src/main/java/capitec/branch/appointment/slots/infrastructure/dao/SlotDBIdGenerator.java
+++ src/main/java/capitec/branch/appointment/slots/infrastructure/dao/SlotDBIdGenerator.java
@@ -2,21 +2,24 @@ package capitec.branch.appointment.slots.infrastructure.dao;
 
 import capitec.branch.appointment.sharekernel.id.IdStore;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.relational.core.mapping.event.BeforeConvertCallback;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
-
 @Component
 @RequiredArgsConstructor
+@Slf4j
 class SlotDBIdGenerator implements BeforeConvertCallback<SlotEntity> {
     private final IdStore idStore;
     @Override
     public SlotEntity onBeforeConvert(SlotEntity entity) {
 
         if(entity.id() == null){
+            log.debug("Generating new ID for SlotEntity:{}", entity);
            return new SlotEntity(UUID.fromString(idStore.getIdList().get(entity.bookingCount()-1)),entity);
         }
+        log.debug("Updating ID for SlotEntity:{}", entity);
         return entity;
 
     }
