8297908ba5b5d0eefd8710694fb6a9258675a6df
tswlun002 <1998tsewu@gmail.com>
2026-03-10 01:10:16 +0200

    Update: add validation checks for email, fullname, OTP code, traceId, and eventType in OTPEmail record

--- src/main/java/capitec/branch/appointment/notification/domain/OTPEmail.java
+++ src/main/java/capitec/branch/appointment/notification/domain/OTPEmail.java
@@ -1,5 +1,9 @@
 package capitec.branch.appointment.notification.domain;
-
+import capitec.branch.appointment.utils.NamesValidator;
+import capitec.branch.appointment.utils.NotBlankEmailValidator;
+import capitec.branch.appointment.utils.OTPCodeValidator;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.util.Assert;
 
 public record OTPEmail(
         String email,
@@ -9,5 +13,16 @@ public record OTPEmail(
         Notification.UserEventType eventType
 )  implements Email<Notification.UserEventType>
 {
+    public OTPEmail {
 
+        Assert.isTrue(NotBlankEmailValidator.isValid(email), "Email is required");
+
+        Assert.isTrue(NamesValidator.isValid(fullname), "Fullname is required");
+
+        Assert.isTrue(OTPCodeValidator.isValid(OTPCode), "Valid OTP Code is required");
+
+        Assert.isTrue(StringUtils.isNotBlank(traceId), "TraceId is required");
+
+        Assert.notNull(eventType, "EventType is required");
+    }
 }
