d55a6d082d4df70f59ffebdc2e86841b60e9def9
tswlun002 <1998tsewu@gmail.com>
2026-03-10 01:09:46 +0200

    Update: add createdAt field and validation checks to ConfirmationEmail record

--- src/main/java/capitec/branch/appointment/notification/domain/ConfirmationEmail.java
+++ src/main/java/capitec/branch/appointment/notification/domain/ConfirmationEmail.java
@@ -1,12 +1,29 @@
 package capitec.branch.appointment.notification.domain;
 
 
+import capitec.branch.appointment.utils.*;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.util.Assert;
 
+import java.time.LocalDateTime;
 
 public record ConfirmationEmail(
         String email,
         String fullname,
         String traceId,
+        LocalDateTime createdAt,
         Notification.UserEventType eventType)  implements Email<Notification.UserEventType>{
 
+    public  ConfirmationEmail{
+        Assert.isTrue(NotBlankEmailValidator.isValid(email), "Email is required");
+
+        Assert.isTrue(NamesValidator.isValid(fullname), "Fullname is required");
+
+        Assert.isTrue(StringUtils.isNotBlank(traceId), "TraceId is required");
+
+        Assert.notNull(eventType, "EventType is required");
+
+        Assert.notNull(createdAt, "CreatedAt is required");
+
+    }
 }
